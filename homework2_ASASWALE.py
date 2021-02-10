1e4b90f8cecea9e49ad9e75f41c738d0f153dc10
Ashay Aswale <asaswale@wpi.edu>
2021-02-10 04:54:04 -0500

    WIP psudo code for double cross validation

--- homework2_ASASWALE.py
+++ homework2_ASASWALE.py
@@ -1,10 +1,6 @@
 import numpy as np
+import math
 import random 
-
-
-no_data = 0
-no_features = 0
-no_data_act = 0 #number of data points actually available for training
 
 
 def random_rearrange (X_tr, y_tr, seed):
@@ -13,9 +9,11 @@ def random_rearrange (X_tr, y_tr, seed):
     np.random.seed(seed)
     np.random.shuffle(y_tr)
 
+## Need regularization here!
 def updateW(X_tr, y_tr, w, b, eps):
+    no_data = X_tr.shape[0]
     brac_term = np.dot(X_tr, w) - y_tr + b
-    grad_f = 1/no_data_act*(np.dot(X_tr.T, brac_term))
+    grad_f = 1/no_data*(np.dot(X_tr.T, brac_term))
     w = w - eps*grad_f
     return w
     
@@ -28,16 +26,17 @@ def updateB(X_tr, y_tr, w, b, eps):
     
 
 def training(X_tr, y_tr, w, b, n_squig, eps, alpha, epochs):
+    no_data = X_tr.shape[0]
     for epoch in range(0, epochs):
         data_remain = True
         n_curr = 0
         n_next = n_squig
         while(data_remain):
-            X_tr_temp = X_tr[n_curr:(min(n_next, no_data_act))]
+            X_tr_temp = X_tr[n_curr:(min(n_next, no_data))]
             n_curr = n_next
             n_next += n_squig
 
-            data_remain = True if n_next<no_data_act else False
+            data_remain = True if n_next<no_data else False
 
             w = updateW(X_tr, y_tr, w, b, eps)
             b = updateB(X_tr, y_tr, w, b, eps)
@@ -45,11 +44,56 @@ def training(X_tr, y_tr, w, b, n_squig, eps, alpha, epochs):
     return w,b
         
 
+def cross_validation(X_tr, y_tr, w, b, validation_perct):
+
+    # 10 folds data logic here
+    # Preferably separate function
+    
+    # for 10 folds:
+        # w, b = training(X_tr_act, y_tr_act, w, b, n_squig, eps, alpha, epochs)
+        # error += test_data(X_tr, y_tr, w, b)
+    # error = error/10  #(10 folds)
+
+    
+    return error
+
+
+def double_cross_validation(X_tr, y_tr, w, b, validation_perct):
+    no_data = X_tr.shape[0]
+    
+    # Defining values:
+    tr_perct = 1-validation_perct
+    no_data_act = round(no_data*tr_perct)
+    
+    minibatch_perct = np.divide(np.array([1, 5, 10, 25]), 100)
+    eps_set = np.array([0.1, 0.01, 0.001, 0.0001])    
+    alpha_set = np.array([0.1, 0.01, 0.001, 0.0001])    
+    epochs_set = np.array([100, 250, 500, 1000])
+    
+    h_star = [minibatch_perct[0], eps_set[0], alpha_set[0], epochs_set[0]]
+
+    # 10 folds data logic here
+    # Preferably separate function
+    # for 10 folds:
+    #     A = +math.inf
+    #     for minibatch in minibatch_perct:
+    #         for eps in eps_set:
+    #             for alpha in alpha_set:
+    #                 for epochs in epochs_set:
+    #                     n_squig = round(no_data_act*minibatch_perct)
+    #                     A_curr = cross_validation(X_fold, y_fold, w, b, validation_perct)
+    #                     if(A_curr<A):
+    #                         A = A_curr
+    #                         h_star = [minibatch, eps, alpha, epochs]
+    
+    # return minibatch, eps, alpha, epochs
+    
+    
+    
+    return n_squig, eps, alpha, epochs
+
 
 def stoch_grad_regression (X_tr, y_tr):
-    global no_data
-    global no_features
-    global no_data_act
 
     no_data = X_tr.shape[0]
     no_features = X_tr.shape[1]
@@ -58,22 +102,11 @@ def stoch_grad_regression (X_tr, y_tr):
     w = np.random.rand(no_features)
     b = random.random()
 
+    validation_perct = 20/100
+
     # Randomizing the data
     random_rearrange(X_tr, y_tr, 10) #seed can be any random number
-
-    # Defining values:
-    validation_perct = 20/100
-    tr_perct = 1-validation_perct
-    no_data_act = round(no_data*tr_perct)
-    minibatch_perct = 10/100
-
-    n_squig = round(no_data_act*minibatch_perct)
-    eps = 0.001
-    alpha = 0.1
-    epochs = 100 #1000 works better, testing error 235
-
-    X_tr_act = X_tr[0:no_data_act]
-    y_tr_act = y_tr[0:no_data_act]
+    n_squig, eps, alpha, epochs = double_cross_validation(X_tr, y_tr, w, b, validation_perct)
     w,b = training(X_tr_act, y_tr_act, w, b, n_squig, eps, alpha, epochs)
     return w,b
     
