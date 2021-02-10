08f28ac15759e26dee0a68453a99136f7d0b058c
Ashay Aswale <asaswale@wpi.edu>
2021-02-10 17:16:11 -0500

    quickrun

--- homework2_ASASWALE.py
+++ homework2_ASASWALE.py
@@ -1,6 +1,7 @@
 import numpy as np
 import math
 import random 
+import copy
 
 
 def random_rearrange (X_tr, y_tr, seed):
@@ -9,19 +10,21 @@ def random_rearrange (X_tr, y_tr, seed):
     np.random.seed(seed)
     np.random.shuffle(y_tr)
 
-## Need regularization here!
-def updateW(X_tr, y_tr, w, b, eps):
+
+def updateW(X_tr, y_tr, w, b, eps, alpha):
     no_data = X_tr.shape[0]
     brac_term = np.dot(X_tr, w) - y_tr + b
-    grad_f = 1/no_data*(np.dot(X_tr.T, brac_term))
-    w = w - eps*grad_f
+    grad_f_mse = 1/no_data*(np.dot(X_tr.T, brac_term))
+    regularisation = alpha/(no_data)*(w)
+    grad_f = grad_f_mse + regularisation
+    w -= eps*grad_f
     return w
     
 
 def updateB(X_tr, y_tr, w, b, eps):
     brac_term = np.dot(X_tr, w) - y_tr + b
     grad_f = np.mean(brac_term)
-    b = b - eps*grad_f
+    b -= eps*grad_f
     return b
     
 
@@ -38,19 +41,44 @@ def training(X_tr, y_tr, w, b, n_squig, eps, alpha, epochs):
 
             data_remain = True if n_next<no_data else False
 
-            w = updateW(X_tr, y_tr, w, b, eps)
+            w = updateW(X_tr, y_tr, w, b, eps, alpha)
             b = updateB(X_tr, y_tr, w, b, eps)
-        print(test_data(X_tr, y_tr, w, b))
+
     return w,b
         
 
-def cross_validation(X_tr, y_tr, w, b, validation_perct):
+def cross_validation(X_tr, y_tr, n_squig, eps, alpha, epochs, no_of_folds):
+    no_data = X_tr.shape[0]
+    no_features = X_tr.shape[1]
+    
+    w = np.random.rand(no_features)
+    b = random.random()
+    no_of_data_per_fold = no_data/no_of_folds   
+    n_curr = 0
+    n_next = no_of_data_per_fold
+    acc = np.zeros([no_of_folds])
+    
+    for k in range(0,no_of_folds):
+        X_tr_local = copy.deepcopy(X_tr)
+        y_tr_local = copy.deepcopy(y_tr)
 
-    # 10 folds data logic here
-    # Preferably separate function
+
+        X_tr_valid = X_tr_local[n_curr:(min(int(n_next), no_data))]
+        y_tr_valid = y_tr_local[n_curr:(min(int(n_next), no_data))]
+
+        X_tr_local = np.delete(X_tr_local, range(n_curr,(min(int(n_next), no_data))), axis = 0)
+        y_tr_local = np.delete(y_tr_local, range(n_curr,(min(int(n_next), no_data))), axis = 0)
+
+        n_curr = int(n_next)
+        n_next += no_of_data_per_fold
+
+        w, b = training(X_tr_local, y_tr_local, w, b, n_squig, eps, alpha, epochs)
+        acc[k] = test_data(X_tr_valid, y_tr_valid, w, b)
+        
+    return np.mean(acc)
+
     
     # for 10 folds:
-        # w, b = training(X_tr_act, y_tr_act, w, b, n_squig, eps, alpha, epochs)
         # error += test_data(X_tr, y_tr, w, b)
     # error = error/10  #(10 folds)
 
@@ -58,40 +86,62 @@ def cross_validation(X_tr, y_tr, w, b, validation_perct):
     return error
 
 
-def double_cross_validation(X_tr, y_tr, w, b, validation_perct):
+def double_cross_validation(X_tr, y_tr):
     no_data = X_tr.shape[0]
-    
-    # Defining values:
-    tr_perct = 1-validation_perct
-    no_data_act = round(no_data*tr_perct)
-    
-    minibatch_perct = np.divide(np.array([1, 5, 10, 25]), 100)
-    eps_set = np.array([0.1, 0.01, 0.001, 0.0001])    
-    alpha_set = np.array([0.1, 0.01, 0.001, 0.0001])    
-    epochs_set = np.array([100, 250, 500, 1000])
-    
-    h_star = [minibatch_perct[0], eps_set[0], alpha_set[0], epochs_set[0]]
+    no_features = X_tr.shape[1]
 
-    # 10 folds data logic here
-    # Preferably separate function
-    # for 10 folds:
-    #     A = +math.inf
-    #     for minibatch in minibatch_perct:
-    #         for eps in eps_set:
-    #             for alpha in alpha_set:
-    #                 for epochs in epochs_set:
-    #                     n_squig = round(no_data_act*minibatch_perct)
-    #                     A_curr = cross_validation(X_fold, y_fold, w, b, validation_perct)
-    #                     if(A_curr<A):
-    #                         A = A_curr
-    #                         h_star = [minibatch, eps, alpha, epochs]
+    # Step 1, random w and b generation
+    w = np.random.rand(no_features)
+    b = random.random()
+        
+    # n_squig_set = np.array([50, 100, 250, 500])
+    n_squig_set = np.array([100, 500])
+    # eps_set = np.array([0.1, 0.01, 0.001, 0.0001])
+    eps_set = np.array([0.001, 0.0001])  
+    # alpha_set = np.array([0.1, 0.01, 0.001, 0.0001])
+    alpha_set = np.array([0.001, 0.0001])
+    # epochs_set = np.array([50, 100, 500, 1000])
+    epochs_set = np.array([1, 5])
     
-    # return minibatch, eps, alpha, epochs
+    h_star = [n_squig_set[0], eps_set[0], alpha_set[0], epochs_set[0]]
+
+    no_of_folds = 2
+    no_of_data_per_fold = no_data/no_of_folds   
+    n_curr = 0
+    n_next = no_of_data_per_fold
     
+    for k in range(0,no_of_folds):
+        X_tr_local = copy.deepcopy(X_tr)
+        y_tr_local = copy.deepcopy(y_tr)
+
+        X_tr_valid = X_tr_local[int(n_curr):(min(int(n_next), no_data))]
+        y_tr_valid = y_tr_local[int(n_curr):(min(int(n_next), no_data))]
+
+        X_tr_local = np.delete(X_tr_local, range(int(n_curr),(min(int(n_next), no_data))), axis = 0)
+        y_tr_local = np.delete(y_tr_local, range(int(n_curr),(min(int(n_next), no_data))), axis = 0)
+
+        n_curr = n_next
+        n_next += no_of_data_per_fold
+
+        A = +math.inf
+        no_data_act = X_tr_local.shape[0]
+        for n_squig in n_squig_set:
+            for eps in eps_set:
+                for alpha in alpha_set:
+                    for epochs in epochs_set:
+                        A_curr = cross_validation(X_tr_local, y_tr_local, n_squig, eps, alpha, epochs, no_of_folds)
+                        print("Error:", A_curr, "h_star:", n_squig, eps, alpha, epochs)
+                        if(A_curr<A):
+                            A = A_curr
+                            h_star = [n_squig, eps, alpha, epochs]
+                            print("## h_star updated ##")
     
-    
+    print("######################")
+    print("BEST HYPERPARAMETERS")
+    print(n_squig, eps, alpha, epochs)
+    print("######################")
     return n_squig, eps, alpha, epochs
-
+    
 
 def stoch_grad_regression (X_tr, y_tr):
 
@@ -99,24 +149,23 @@ def stoch_grad_regression (X_tr, y_tr):
     no_features = X_tr.shape[1]
 
     # Step 1, random w and b generation
-    w = np.random.rand(no_features)
-    b = random.random()
 
     validation_perct = 20/100
 
     # Randomizing the data
     random_rearrange(X_tr, y_tr, 10) #seed can be any random number
-    n_squig, eps, alpha, epochs = double_cross_validation(X_tr, y_tr, w, b, validation_perct)
-    w,b = training(X_tr_act, y_tr_act, w, b, n_squig, eps, alpha, epochs)
+    n_squig, eps, alpha, epochs = double_cross_validation(X_tr, y_tr)
+
+    w = np.random.rand(no_features)
+    b = random.random()
+    w,b = training(X_tr, y_tr, w, b, n_squig, eps, alpha, epochs)
     return w,b
     
 
 def test_data(X_te, y_te, w, b):
-    f = 0
-    for i, x in enumerate(X_te):
-        f += (np.dot(x,w) - y_te[i] + b)**2
-    f = f/(X_te.shape[0])
-    return f
+    f = np.square(np.dot(X_te, w)-y_te + b)
+    err = np.mean(f)/2
+    return err
 
 
     
@@ -129,8 +178,9 @@ def train_age_regressor ():# train_age_regressor()
 
     w,b = stoch_grad_regression(X_tr, y_tr)
     testing_age = test_data(X_te, y_te, w, b)
-    print(test_data(X_te, y_te, w, b))
-
+    print("################################")
+    print("FINAL TESTING ERROR:", testing_age)
+    print("################################")
     
 
     # Report fMSE cost on the training and testing data (separately)
